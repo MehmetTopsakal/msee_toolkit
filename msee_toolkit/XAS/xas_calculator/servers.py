3b1869c8afc628821ba8277705dfa0e871fe7cca
MEHMET TOPSAKAL <metokal@gmail.com>
2022-08-05 09:50:24 -0400

    bav fix

--- msee_toolkit/XAS/xas_calculator/servers.py
+++ msee_toolkit/XAS/xas_calculator/servers.py
@@ -181,88 +181,94 @@ def feff_calculator_mpi(js):
         print(exc)
 
 
+
+
 def fdmnes_calculator_mpi(js):
-    """
+    '''
     # USAGE:
     with open('/data/software/FDMNES/fdmnes_tests/4_nospin/fdmnes.inp') as fi:
         fdmnesinp = fi.readlines()
-    js =  {'task':'fdmnes_run', 'fdmnes_inp': fdmnesinp, 'ncores':12}
+    js =  {'task':'fdmnes_run', 'fdmnes_inp': fdmnesinp, 'ncores':12} 
     js_out = fdmnes_calculator_mpi(js)
     js_out
-    """
-
-    # runtime parameters
+    '''
+    
+    #runtime parameters
     try:
-        ncores = js["ncores"]
+        ncores = js['ncores']
     except:
-        ncores = 4
+        ncores=4
 
+        
     try:
-        fdmnes_scratch_path = js["fdmnes_scratch_path"]
+        fdmnes_scratch_path = js['fdmnes_scratch_path']
     except:
-        fdmnes_scratch_path = "/data/scratch/zmq_servers/FDMNES/"
-
+        fdmnes_scratch_path = '/data/scratch/zmq_servers/FDMNES/'        
+        
     try:
-        mpirun_cmd = js["mpirun_cmd"]
+        mpirun_cmd = js['mpirun_cmd']
     except:
-        mpirun_cmd = "/opt/intel/oneapi/mpi/2021.5.1/bin/mpirun"
-
+        mpirun_cmd = '/opt/intel/oneapi/mpi/2021.5.1/bin/mpirun' 
+    
     try:
-        exe_path = js["exe_path"]
+        exe_path = js['exe_path']
     except:
-        exe_path = "/data/software/FDMNES/parallel_fdmnes"
+        exe_path = '/data/software/FDMNES/parallel_fdmnes'
 
     try:
         mumps = js["HOST_NUM_FOR_MUMPS"]
     except:
         mumps = 1
-
+        
     try:
-        cleanup = js["cleanup"]
+        cleanup = js['cleanup']
     except:
-        cleanup = "true"
-
+        cleanup = 'false'
+        
+    
+    
+    
     try:
 
         os.chdir(fdmnes_scratch_path)
-        uid = "fdmnes_" + "".join(
-            random.choices(string.ascii_uppercase + string.ascii_lowercase, k=10)
-        )
-        os.makedirs(uid, exist_ok=True)
+        uid = 'fdmnes_'+''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=10))
+        os.makedirs(uid,exist_ok=True)
         os.chdir(uid)
-
-        print(
-            "Running FDMNES calculation at \n %s/%s \n using %d cores\n"
-            % (fdmnes_scratch_path, uid, ncores)
-        )
-
+        
+        print('Running FDMNES calculation at \n %s/%s \n using %d cores\n'%(fdmnes_scratch_path,uid,ncores))
+        
         fi = open("fdmnes.inp", "a")
-        for ii in js["fdmnes_inp"]:
+        for ii in js['fdmnes_inp']:
             fi.write(ii)
         fi.close()
+        
 
         try:
-            js["cif"]
+            js['cif']
             cif = open("structure.cif", "a")
-            for cc in js["cif"]:
+            for cc in js['cif']:
                 cif.write(cc)
             cif.close()
         except:
             pass
 
-        # fdmnes wants this
-        fi = open("fdmfile.txt", "a")
-        fi.write("1\n")
-        fi.write("fdmnes.inp\n")
-        fi.close()
 
+
+
+        #fdmnes wants this
+        fi = open("fdmfile.txt", "a")
+        fi.write('1\n')
+        fi.write('fdmnes.inp\n')
+        fi.close()          
+        
         exe_list = [
-            "fdmnes_mpi_linux64",
+            'fdmnes_mpi_linux64',
         ]
 
-        start_time = time.time()
+        start_time = time.time()  
 
-        open("fdmnes.out", "a").close()
+        open('fdmnes.out', 'a' ).close()
+
         for e in exe_list:
             _ = subprocess.run(
                 [
@@ -273,128 +279,132 @@ def fdmnes_calculator_mpi(js):
             )
 
             if _.returncode > 0:
-                print("error at %s" % e)
+                print('error at %s'%e)
                 print(_)
                 break
         finish_time = time.time()
+        
 
-        with open("fdmnes.inp") as f:
+        
+      
+
+        with open('fdmnes.inp') as f:
             fdmnesinp = f.readlines()
-        with open("fdmnes.out") as f:
+        with open('fdmnes.out') as f:
             fdmnesout = f.readlines()
-        with open("fdmnes_bav.out") as f:
+        with open('fdmnes_bav.txt') as f:
             fdmnesbav = f.readlines()
 
         js = {
-            "start_time": datetime.datetime.fromtimestamp(start_time).strftime(
-                "%Y-%m-%d__%H:%M:%S"
-            ),
-            "finish_time": datetime.datetime.fromtimestamp(finish_time).strftime(
-                "%Y-%m-%d__%H:%M:%S"
-            ),
-            "time_elapsed": finish_time - start_time,
-            "fdmnesinp": fdmnesinp,
-            "fdmnesout": fdmnesout,
-            "fdmnesbav": fdmnesbav,
-            "mpirun_cmd": mpirun_cmd,
-            "ncores": ncores,
-            "exe_path": exe_path,
-        }
+            'start_time': datetime.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d__%H:%M:%S'),
+            'finish_time': datetime.datetime.fromtimestamp(finish_time).strftime('%Y-%m-%d__%H:%M:%S'),
+            'time_elapsed': finish_time - start_time,
+            'fdmnesinp': fdmnesinp,
+            'fdmnesout': fdmnesout,
+            'fdmnesbav': fdmnesbav,
+            'mpirun_cmd': mpirun_cmd,
+            'ncores': ncores,
+            'exe_path': exe_path,
+            }            
+            
+
 
         # single site
-        if os.path.isfile("fdmnes.txt"):
-            with open("fdmnes.txt") as f:
+        if os.path.isfile('fdmnes.txt'):
+            with open('fdmnes.txt') as f:
                 fdmnestxt = f.readlines()
             # non-magnetic calculation
             if len(fdmnestxt[-1].split()) == 2:
                 js_new = {
-                    "fname_fdmnestxt": "fdmnes.txt",
-                    "header_fdmnestxt": fdmnestxt[0:2],
-                    "e": [float(i.split()[0]) for i in fdmnestxt[2:]],
-                    "mu": [float(i.split()[1]) for i in fdmnestxt[2:]],
-                }
+                    'fname_fdmnestxt': 'fdmnes.txt',
+                    'header_fdmnestxt': fdmnestxt[0:2],
+                    'e':     [float(i.split()[0]) for i in fdmnestxt[2:]],
+                    'mu':    [float(i.split()[1]) for i in fdmnestxt[2:]],
+                    }
                 js.update(js_new)
             # magnetic calculation
             elif len(fdmnestxt[-1].split()) == 3:
                 js_new = {
-                    "fname_fdmnestxt": "fdmnes.txt",
-                    "header_fdmnestxt": fdmnestxt[0:2],
-                    "e": [float(i.split()[0]) for i in fdmnestxt[2:]],
-                    "mu_up": [float(i.split()[1]) for i in fdmnestxt[2:]],
-                    "mu_dw": [float(i.split()[2]) for i in fdmnestxt[2:]],
-                    "mu": [
-                        (float(i.split()[1]) + float(i.split()[2]))
-                        for i in fdmnestxt[2:]
-                    ],
-                }
+                    'fname_fdmnestxt': 'fdmnes.txt',
+                    'header_fdmnestxt': fdmnestxt[0:2],
+                    'e':     [float(i.split()[0]) for i in fdmnestxt[2:]],
+                    'mu_up':  [float(i.split()[1]) for i in fdmnestxt[2:]],
+                    'mu_dw':  [float(i.split()[2]) for i in fdmnestxt[2:]],
+                    'mu':  [(float(i.split()[1])+float(i.split()[2])) for i in fdmnestxt[2:]],
+                    }
                 js.update(js_new)
 
+
         # multiple sites
-        elif os.path.isfile("fdmnes_1.txt"):
+        else:
 
-            import glob
-
-            outputs = glob.glob("fdmnes_*.txt")
+            import glob 
+            outputs = glob.glob('fdmnes_*.txt')
 
             for i in outputs:
                 try:
-                    t = int(i.split("_")[1].split(".txt")[0])
-
-                    with open("fdmnes_%d.txt" % t) as f:
+                    t = int(i.split('_')[1].split('.txt')[0])
+                    
+                    with open('fdmnes_%d.txt'%t) as f:
                         fdmnestxt = f.readlines()
-
+                            
                     # non-magnetic calculation
                     if len(fdmnestxt[-1].split()) == 2:
                         js_new = {
-                            "fname_fdmnestxt_%d" % t: i,
-                            "header_fdmnestxt_%d" % t: fdmnestxt[0:2],
-                            "e_%d" % t: [float(i.split()[0]) for i in fdmnestxt[2:]],
-                            "mu_%d" % t: [float(i.split()[1]) for i in fdmnestxt[2:]],
-                        }
+                            'fname_fdmnestxt_%d'%t: i,
+                            'header_fdmnestxt_%d'%t: fdmnestxt[0:2],
+                            'e_%d'%t:     [float(i.split()[0]) for i in fdmnestxt[2:]],
+                            'mu_%d'%t:    [float(i.split()[1]) for i in fdmnestxt[2:]],
+                            }
                         js.update(js_new)
 
                     # magnetic calculation
                     elif len(fdmnestxt[-1].split()) == 3:
                         js_new = {
-                            "fname_fdmnestxt_%d" % t: i,
-                            "header_fdmnestxt_%d" % t: fdmnestxt[0:2],
-                            "e_%d" % t: [float(i.split()[0]) for i in fdmnestxt[2:]],
-                            "mu_up_%d"
-                            % t: [float(i.split()[1]) for i in fdmnestxt[2:]],
-                            "mu_dw_%d"
-                            % t: [float(i.split()[2]) for i in fdmnestxt[2:]],
-                            "mu_%d"
-                            % t: [
-                                (float(i.split()[1]) + float(i.split()[2]))
-                                for i in fdmnestxt[2:]
-                            ],
-                        }
-                        js.update(js_new)
+                            'fname_fdmnestxt_%d'%t: i,
+                            'header_fdmnestxt_%d'%t: fdmnestxt[0:2],
+                            'e_%d'%t:     [float(i.split()[0]) for i in fdmnestxt[2:]],
+                            'mu_up_%d'%t:  [float(i.split()[1]) for i in fdmnestxt[2:]],
+                            'mu_dw_%d'%t:  [float(i.split()[2]) for i in fdmnestxt[2:]],
+                            'mu_%d'%t:  [(float(i.split()[1])+float(i.split()[2])) for i in fdmnestxt[2:]],
+                            }
+                        js.update(js_new)  
 
                 except:
-                    pass
+                    pass            
 
-        if os.path.isfile("fdmnes_conv.txt"):
-            with open("fdmnes_conv.txt") as f:
+
+
+
+
+            
+        if os.path.isfile('fdmnes_conv.txt'):
+            with open('fdmnes_conv.txt') as f:
                 fdmnestxt_conv = f.readlines()
             js_new = {
-                "fname_fdmnesconvtxt": "fdmnes_conv.txt",
-                "header_fdmnesconvtxt": fdmnestxt[0:1],
-                "e_conv": [float(i.split()[0]) for i in fdmnestxt_conv[1:]],
-                "mu_conv": [float(i.split()[1]) for i in fdmnestxt_conv[1:]],
-            }
+                'fname_fdmnesconvtxt': 'fdmnes_conv.txt',
+                'header_fdmnesconvtxt': fdmnestxt[0:1],
+                'e_conv':     [float(i.split()[0]) for i in fdmnestxt_conv[1:]],
+                'mu_conv':    [float(i.split()[1]) for i in fdmnestxt_conv[1:]],
+                }
             js.update(js_new)
 
-        os.chdir("..")
 
-        if cleanup == "true":
+        os.chdir('..')
+
+        if cleanup == 'true':
             shutil.rmtree(uid)
-
+            
+            
         return js
 
+
     except Exception as exc:
-        print("something is wrong")
+        print('something is wrong')
         print(exc)
+
+
+
 
 
 while True:
